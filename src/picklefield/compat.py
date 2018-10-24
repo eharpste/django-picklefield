15602dedbfb1fa80756c1d5c28aa7dbdc3c5459f
eharpste <eharpste@cs.cmu.edu>
2018-10-24 12:24:44 -0400

    changd the compat file to reference dill instead of pickle, I'm hoping that's all that needs to be touched...

--- src/picklefield/compat.py
+++ src/picklefield/compat.py
@@ -1,6 +1,8 @@
 # python 3.x does not have cPickle module
-try:
-    # cpython 2.x
-    from cPickle import loads, dumps  # noqa
-except ImportError:
-    from pickle import loads, dumps  # noqa
+# try:
+#     # cpython 2.x
+#     from cPickle import loads, dumps  # noqa
+# except ImportError:
+#     from pickle import loads, dumps  # noqa
+
+from dill import loads, dumps
